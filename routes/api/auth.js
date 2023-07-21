18539ea6e8950b8fdf93792ff845397b112118cc
Sophia <sophia@MacBook-Air-Sophia.local>
2023-07-21 11:13:19 +0300

    hw5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { validateBody } = require("../../middlewares/validateBody");
 const { schemas } = require("../../models/user");
 const { authenticate } = require("../../middlewares/authenticate");
+const { upload } = require("../../middlewares/upload");
 const ctrl = require("../../controllers/auth");
 const router = express.Router();
 
@@ -11,9 +12,16 @@ router.patch(
   validateBody(schemas.updateSubscriptionSchema),
   ctrl.updateSubscriptionUser
 );
-router.post("/register", validateBody(schemas.authSchema), ctrl.register);
-router.post("/login", validateBody(schemas.authSchema), ctrl.login);
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.post("/logout", authenticate, ctrl.logout);
 router.get("/current", authenticate, ctrl.current);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
